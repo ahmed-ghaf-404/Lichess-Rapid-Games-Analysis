fefbeaf56af2f32927fe934bd3ceeea571b97eff
Ahmed Alghafri <alghaf.ahmd@gmail.com>
2022-01-01 23:14:52 +0400

    added more functions

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-import generate_dataset as gen
+import generate_dataset as Gen
+import pandas
 
 
 if __name__ == "__main__":
-    username_set = set()
-    username_set.add("ChocoRoku")
+    username_set = Gen.read_usernames("Data/Usernames/kaggle_usernames.pkl")
 
-    gen.scrape(username_set, "Data")
+    # username_set = set()
+    # username_set.add("ChocoRoku")
+
+    more_usernames = Gen.scrape(username_set, "Data/Games")
+
+    pandas.DataFrame.to_csv(Gen.generate_final_dataset(), "Data/CSV_Files/0.csv")
