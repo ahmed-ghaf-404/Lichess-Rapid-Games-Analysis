424459850ef267bdcf0dec1214d35d9aedec55f5
Ahmed Alghafri <alghaf.ahmd@gmail.com>
2022-08-30 10:19:19 -0400

    Added 6000 games

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import _utility as util
 
 def main():
@@ -5,12 +6,16 @@ def main():
 
     username_csv_file_path = "Data/Prepared_Data/Initial_Usernames.csv"
     username_set = util.getUsernameSet(username_csv_file_path)
-
+    count = 0
     for username in username_set:
         try:
             util.scrape_n_rapid_games(username,5) 
         except Exception as e:
             continue 
+        count +=1
+        if count == 8:
+            time.sleep(3)
+            count = 0
 
     util.buildDataset('Data/Scraped_Files')
 
