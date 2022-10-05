2aa1b65e0e3e4e91970e2aa404b96284e8a33728
Ahmed Alghafri <50543745+ahmed-ghaf-404@users.noreply.github.com>
2022-10-05 02:39:13 -0400

    fixed reckless file changes

--- Chess Level Guesser/_utility.py
+++ Chess Level Guesser/_utility.py
@@ -96,13 +96,28 @@ def deleteGame(path):
 def validateGame(path):
     with open(path, 'r') as f:
         game = json.load(f)
-        if game['variant']!='rapid':
+        if game['variant']!='standard':
             # sus game
             filename = path.split('/')[-1]
             os.rename(path, f'./Data/SusGames/{filename}')
             return False
     
     return True
+def returnGame(path):
+    with open(path, 'r') as f:
+        game = json.load(f)
+        filename = path.split('/')[-1]
+        white = game['players']['white']['user']['name']
+        if not os.path.exists(f'Data/Scraped_Files/{white}'):
+            os.mkdir(f'Data/Scraped_Files/{white}')
+        os.rename(path, f'Data/Scraped_Files/{white}/{filename}')
+        
+    
+    return True
 
 if __name__ == '__main__':
-    validateGame("./Data/Scraped_Files/RatchetLodross/fOBrhnyQ.json")
+    validateGame("Data/Scraped_Files/RatchetLodross/fOBrhnyQ.json")
+    for subdir, dirs, files in os.walk('Data/SusGames'):
+        for file in files:
+            print(os.path.join(subdir, file))
+            returnGame(os.path.join(subdir, file))
