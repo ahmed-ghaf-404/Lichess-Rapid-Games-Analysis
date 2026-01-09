e54905ae98e543650c79e185fad0384e2abfc268
ahmed-ghaf-404 <alghaf.ahmd@gmail.com>
2026-01-10 03:40:52 +0400

    updated lint

--- lichess_rapid_games_analysis/Helper.py
+++ lichess_rapid_games_analysis/Helper.py
@@ -5,107 +5,148 @@ import json
 import os
 from DatabaseManager import DatabaseManager
 
+
 class Helper:
     def __init__(self, databaseManager):
         self.dbManager = databaseManager
 
-    def getUsernameSet(self,csv_path: str):
-        '''
-        PARAM: 
+    def getUsernameSet(self, csv_path: str):
+        """
+        PARAM:
             csv_path String: path to the CSV file containing usernames
-        
+
         RETURN:
             username_set Set: contains all usernames we know
-        '''
+        """
         csv = pd.read_csv(csv_path)
         username_set = set()
         for index, row in csv.iterrows():
-            username_set.add(row['Username'])    
+            username_set.add(row["Username"])
         return username_set
 
-    def createUsernameCSV(self,username_set: set):
-        '''
+    def createUsernameCSV(self, username_set: set):
+        """
         PARAM:
-        username_set Set: set containing all usernames we have so far. 
-        
+        username_set Set: set containing all usernames we have so far.
+
         Creates a new CSV file
 
         RETURN:
         None
-        '''
+        """
         df = pd.DataFrame()
-        df['Username'] = list(username_set)
-        df.to_csv('Data/Prepared_Data/Usernames-{}.csv'.format(datetime.datetime.now()), index=False)
-        return 
+        df["Username"] = list(username_set)
+        df.to_csv(
+            "Data/Prepared_Data/Usernames-{}.csv".format(
+                datetime.datetime.now()
+            ),
+            index=False,
+        )
+        return
 
-    def scrape_n_rapid_games(   self, 
-                                path:str, 
-                                username:str, 
-                                num_of_games:int=1, 
-                                allow_duplicate=False, 
-                                is_need_more_players=False):
-        '''
+    def scrape_n_rapid_games(
+        self,
+        path: str,
+        username: str,
+        num_of_games: int = 1,
+        allow_duplicate=False,
+        is_need_more_players=False,
+    ):
+        """
         PARAM:
             username String: a lichess username
             num_of_games int: number of games to scrape
             no_duplicate bool: whether you want to mine an already existing username.
             Creates a JSON file containing the game review data
-        '''
+        """
         if not allow_duplicate and username in C.SCRAPED_USERNAMES:
             print("{} has already been mined".format(username))
-            return 
+            return
         game_added_counter = 0
         new_username = set()
         # print('scraping {} game(s) the username: {}'.format(num_of_games, username))
         try:
-            gameLists = C.CLIENT.games.export_by_player(username=username, as_pgn=False, max=num_of_games,rated=True,perf_type="rapid", analysed=True, moves=True, evals=True,opening=True)
+            gameLists = C.CLIENT.games.export_by_player(
+                username=username,
+                as_pgn=False,
+                max=num_of_games,
+                rated=True,
+                perf_type="rapid",
+                analysed=True,
+                moves=True,
+                evals=True,
+                opening=True,
+            )
             for game in gameLists:
-                if not os.path.isdir("{}/{}".format(path,username)):
-                    os.mkdir("{}/{}".format(path,username))
+                if not os.path.isdir("{}/{}".format(path, username)):
+                    os.mkdir("{}/{}".format(path, username))
                 if os.path.isfile(f"{path}/{username}/{game['id']}.json"):
-                    if os.path.getsize(f"{path}/{username}/{game['id']}.json") == 0:
+                    if (
+                        os.path.getsize(f"{path}/{username}/{game['id']}.json")
+                        == 0
+                    ):
                         os.remove(f"{path}/{username}/{game['id']}.json")
                     continue
-                
+
                 if is_need_more_players:
-                    if username.lower() == game["players"]["white"]["user"]["name"].lower():
-                        new_username.add(game["players"]["black"]["user"]["name"])
+                    if (
+                        username.lower()
+                        == game["players"]["white"]["user"]["name"].lower()
+                    ):
+                        new_username.add(
+                            game["players"]["black"]["user"]["name"]
+                        )
                     else:
-                        new_username.add(game["players"]["white"]["user"]["name"])
-                with open(f"{path}/{username}/{game['id']}.json", 'w+') as f:
+                        new_username.add(
+                            game["players"]["white"]["user"]["name"]
+                        )
+                with open(f"{path}/{username}/{game['id']}.json", "w+") as f:
                     json.dump(game, f, indent=4, sort_keys=False, default=str)
                 if self.validateGame(f"{path}/{username}/{game['id']}.json"):
                     game_added_counter += 1
                 else:
                     os.remove(f"{path}/{username}/{game['id']}.json")
-                    
+
         except Exception as e:
             print(e)
         print(f"Added {game_added_counter}/{num_of_games} to {username}")
         return new_username
 
-    def validateGame(self, path:str):
+    def validateGame(self, path: str):
         if os.path.getsize(path) == 0:
             os.remove(path)
             print("empty file removed")
             return False
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             game = json.load(f)
-            if (game['variant']!='standard' or
-                'opening' not in game or 
-                'provisional' in game['players']['white'] or
-                'provisional' in game['players']['black'] or
-                abs(game['players']['white']['rating'] - game['players']['black']['rating']) > 100 or 
-                len(game['moves'].split(' ')) < 40):    
+            if (
+                game["variant"] != "standard"
+                or "opening" not in game
+                or "provisional" in game["players"]["white"]
+                or "provisional" in game["players"]["black"]
+                or abs(
+                    game["players"]["white"]["rating"]
+                    - game["players"]["black"]["rating"]
+                )
+                > 100
+                or len(game["moves"].split(" ")) < 40
+            ):
                 return False
-            if 'ratingDiff' in game['players']['white']:
-                if game['players']['white']['ratingDiff'] + game['players']['black']['ratingDiff'] > 2:
+            if "ratingDiff" in game["players"]["white"]:
+                if (
+                    game["players"]["white"]["ratingDiff"]
+                    + game["players"]["black"]["ratingDiff"]
+                    > 2
+                ):
                     return False
-                    
+
         return True
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     helper = Helper(DatabaseManager())
-    print(helper.scrape_n_rapid_games("Data/Scraped_Files", "ericrosen",50,True,True))
-    
-        
+    print(
+        helper.scrape_n_rapid_games(
+            "Data/Scraped_Files", "ericrosen", 50, True, True
+        )
+    )
--- lichess_rapid_games_analysis/__main__.py
+++ lichess_rapid_games_analysis/__main__.py
@@ -2,49 +2,53 @@ import time
 import _utility as util
 from DatabaseManager import DatabaseManager
 
+
 def main():
     # start the application
     print("Welcome to Lichess Rapid Games Analysis.")
 
     # connect to Database
-    dbManager = DatabaseManager() 
+    dbManager = DatabaseManager()
 
     choice = input(
-    '''Main Menu:
+        """Main Menu:
         1 Database stats
-    ''')
+    """
+    )
 
     match choice:
-        case '1':
+        case "1":
             dbManager.countQuery()
 
     # testing stuff
     username_csv_file_path = "Data/Prepared_Data/Initial_Usernames.csv"
-    
-    isEdit = input(f"Current data path is {username_csv_file_path}\nPress E to edit")
-    if isEdit.lower() == 'e':
-        while isEdit.lower() == 'e':
-            isEdit = input(f"Current data path is {username_csv_file_path}\nPress E to edit")
 
+    isEdit = input(
+        f"Current data path is {username_csv_file_path}\nPress E to edit"
+    )
+    if isEdit.lower() == "e":
+        while isEdit.lower() == "e":
+            isEdit = input(
+                f"Current data path is {username_csv_file_path}\nPress E to edit"
+            )
 
     username_set = util.getUsernameSet(username_csv_file_path)
     count = 0
     for username in username_set:
         try:
-            util.scrape_n_rapid_games(username,5) 
+            util.scrape_n_rapid_games(username, 5)
         except Exception as e:
-            continue 
-        count +=1
+            continue
+        count += 1
         if count == 10:
             time.sleep(1)
             count = 0
 
-    util.buildDataset('Data/Scraped_Files')
+    util.buildDataset("Data/Scraped_Files")
 
     dbManager.closeDatabase()
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
